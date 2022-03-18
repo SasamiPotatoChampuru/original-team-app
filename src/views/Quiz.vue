f3404a75f9980d7aa851d4b1a137753226121049
manacat1999 <manafee1104@gmail.com>
2022-03-18 10:13:16 +0900

    じゅんぱいがきた

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,91 +1,96 @@
 <template>
-  <div class="app">
-    <p>参加人数を選択してください</p>
+  <div v-if="first">
+    <div class="app">
+      <p>参加人数を選択してください</p>
 
-    <div class="suji">
-      <button v-on:click="showQuiz(3)">3</button>
-      <button v-on:click="showQuiz(4)">4</button>
-      <button v-on:click="showQuiz(5)">5</button>
-      <button v-on:click="showQuiz(6)">6</button>
-      <button v-on:click="showQuiz(7)">7</button>
-      <button v-on:click="showQuiz(8)">8</button>
-      <button v-on:click="showQuiz(9)">9</button>
-      <button v-on:click="showQuiz(10)">10</button>
+      <div class="suji">
+        <button v-on:click="showQuiz(3)">3</button>
+        <button v-on:click="showQuiz(4)">4</button>
+        <button v-on:click="showQuiz(5)">5</button>
+        <button v-on:click="showQuiz(6)">6</button>
+        <button v-on:click="showQuiz(7)">7</button>
+        <button v-on:click="showQuiz(8)">8</button>
+        <button v-on:click="showQuiz(9)">9</button>
+        <button v-on:click="showQuiz(10)">10</button>
+      </div>
+
+      <input
+        type="text"
+        v-if="ifNumber[0]"
+        v-model="inputAnswer0"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[1]"
+        v-model="inputAnswer1"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[2]"
+        v-model="inputAnswer2"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[3]"
+        v-model="inputAnswer3"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[4]"
+        v-model="inputAnswer4"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[5]"
+        v-model="inputAnswer5"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[6]"
+        v-model="inputAnswer6"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[7]"
+        v-model="inputAnswer7"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[8]"
+        v-model="inputAnswer8"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        v-if="ifNumber[9]"
+        v-model="inputAnswer9"
+        maxlength="1"
+      />
     </div>
-    <input
-      type="text"
-      v-if="ifNumber[0]"
-      v-model="inputAnswer0"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[1]"
-      v-model="inputAnswer1"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[2]"
-      v-model="inputAnswer2"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[3]"
-      v-model="inputAnswer3"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[4]"
-      v-model="inputAnswer4"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[5]"
-      v-model="inputAnswer5"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[6]"
-      v-model="inputAnswer6"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[7]"
-      v-model="inputAnswer7"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[8]"
-      v-model="inputAnswer8"
-      maxlength="1"
-    />
-    <input
-      type="text"
-      v-if="ifNumber[9]"
-      v-model="inputAnswer9"
-      maxlength="1"
-    />
 
-    <div v-if="first">
+    <div v-if="second">
       <h2>Q. {{ NwordQuiz[quiznumber].text }}</h2>
 
       <div id="v-model">
         <div>{{ textfield }}</div>
         <button v-on:click="feedback">OK</button>
-
-        <div>{{ feedbacktext }}</div>
-
-        <h1>答え{{ correctanswer }}</h1>
-
-        <h3>スコア{{ quizzes.score }}</h3>
       </div>
+    </div>
+
+    <div v-if="third">
+      <div>{{ feedbacktext }}</div>
+
+      <h1>答え{{ correctanswer }}</h1>
+
+      <h3>スコア{{ quizzes.score }}</h3>
     </div>
   </div>
 </template>
@@ -187,6 +192,17 @@ export default {
         this.feedbacktext = "不正解です！"
       }
     },
+  },
+  feedback: function () {
+    if (this.input == this.words[this.number]) {
+      this.first = false
+      this.second = true
+      this.third = false
+    } else {
+      this.first = false
+      this.second = false
+      this.third = false
+    }
   },
 }
 </script>
